2af35ae572caa24d5a0945abb2e9dee611802791
Dave Beck <dacb@uw.edu>
2019-06-11 10:32:40 -0700

    renamed wos test and and added argument handling

---
+++ scripts/wos_fetch.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import click
+
+from wos import WosClient
+import wos.utils
+
+@click.command()
+@click.option('-o', '--output_file', 'output_file', type=str, required=True,
+        help='name of the output file')
+@click.option('-u', '--username', 'username', type=str, required=True,
+        help='username for accessing the web of science API')
+@click.option('-p', '--password', 'password', type=str, required=True,
+        help='password for accessing the web of science API')
+def wos_fetch(output_file, username, password):
+    with WosClient(username, password) as client:
+        print(wos.utils.query(client, 'AU=Beckner Wesley'))
+
+
+__name__ == '__main__':
+    wos_fetch()
--- wos.test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-from wos import WosClient
-import wos.utils
-
-with WosClient() as client:
-    print(wos.utils.query(client, 'AU=Beckner Wesley'))
