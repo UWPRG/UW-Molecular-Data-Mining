fd94cca8c3cb09fa3c6693ba42f4424ecb07c5e3
Dave Beck <dacb@uw.edu>
2019-06-27 10:36:13 -0700

    added command line argument handlind, refactored code
    Added command line argument handling with @click.  You may need to
    conda install click.  It takes several options. To see them,
    run the command with either:
    python selenium_extractor_2.py --help
    OR
    ./selenium_extractor_2.py --help
    
    In addition, the code has been refactored and duplicate and unused
    lines removed and comments brought roughly into PEP8 spec.

--- scripts/selenium_extractor_2.py
+++ scripts/selenium_extractor_2.py
@@ -1,88 +1,123 @@
+#!/usr/bin/env python
+
+import time
+
+import click
 import numpy as np
 import pandas as pd
-import time
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-# Some global variables
-first_link = "https://scholar.google.com/scholar?start=0&q=flame+retardant&hl=en&as_sdt=0,48&as_ylo=2015"
-our_html_links = []
 pg_counter = 1
-wait_time = 6   # changes how long you'd like to wait between flipping pages on Google
 
-def first_page(link,output):
-    driver = webdriver.Chrome()                             # opens the chrome webdriver
-    driver.get(link)                                        # heads to chrome, goes to page
-    elems = scrape(driver)                                  # grabs all elements with an href
+def get_first_page(url, output):
+    # opens the chrome webdriver
+    driver = webdriver.Chrome()
+    # heads to chrome, goes to page
+    driver.get(url)
+    # grabs all elements with an href
+    elems = scrape(driver)
     clean_elems = clean(elems)
     for item in clean_elems:
-        if type(link) is str:
-            output.write(link)
+        if type(url) is str:
+            output.write(url)
             output.write('\n')
     return driver, clean_elems
 
-def clean(raw_elems):                                       # input the list of raw xpath elements, filters bad links, returns updated html list
+
+def clean(raw_elems):
+    """
+    input the list of raw xpath elements, filters bad urls, returns updated html list
+    """
     hits = []
+    urls = []
     for index, elem in enumerate(raw_elems):
-        link = elem.get_attribute("href")
+        url = elem.get_attribute("href")
 
-        if 'scholar' not in link and 'void' not in link and\
-                                     'pdf' not in link and\
-                                     'php' not in link and\
-                                     'google' not in link and\
-                                     'utm' not in link:
+        if 'scholar' not in url and 'void' not in url and\
+                                     'pdf' not in url and\
+                                     'php' not in url and\
+                                     'google' not in url and\
+                                     'utm' not in url:
             hits.append(index)
-            if len(hits) > 1:                               #after filtering for 'scholar' and 'void' we get the two main links of each article,
-                if index - hits[-2] > 5:                    #the page links and the full article links, which appear w/in 5 indices (usually)
-                    our_html_links.append(link)
+            # after filtering for 'scholar' and 'void' we get the two main urls of each article,
+            if len(hits) > 1:
+                # the page urls and the full article urls, which appear w/in 5 indices (usually)
+                if index - hits[-2] > 5:
+                    urls.append(url)
 
-    return our_html_links
+    return urls
 
-def n_pages(driver,max_iters,counter,pg_start,output):              # This method navigates through a desired number of pages on google, scrapes each one
-    pg_counter = pg_start                                           # After scraping, the newly scraped htmls are written to the output file
+
+def n_pages(driver, max_iters, counter, pg_start, output, wait_time):
+    """
+    This method navigates through a desired number of pages on google, scrapes each one
+    After scraping, the newly scraped htmls are written to the output file
+    """
+    pg_counter = pg_start
+    urls = []
     while counter <= max_iters:
-        time.sleep(wait_time)                                       # throttle so Google doesn't think we're robots
+        # throttle so Google doesn't think we're robots
+        time.sleep(wait_time)
         counter += 1
-        pgs = driver.find_elements_by_class_name('gs_nma')          # finds a list of the buttons which navigate through the pages
+        # finds a list of the buttons which navigate through the pages
+        pgs = driver.find_elements_by_class_name('gs_nma')
 
-        if pg_counter < 7:                                          # google scholar page format changes slightly upon reaching page 7
-            nxt_link = str(pgs[pg_counter].get_attribute('href'))   # grabs the link to the very next page
-            driver.get(nxt_link)
-            elems = scrape(driver)
-            clean_elems = clean(elems)
-            our_html_links.append(clean_elems)
-            for link in clean_elems:                                # loops through cleaned link list, writes to output file
-                if type(link) is str:
-                    output.write(link)
-                    output.write('\n')
-            pg_counter += 1
-            print('Size HTML list:  ',len(our_html_links))          # only do this if you want to keep track of the master list size
-
+        # google scholar page format changes slightly upon reaching page 7
+        if pg_counter < 7:
+            # grabs the url to the very next page
+            nxt_url = str(pgs[pg_counter].get_attribute('href'))
         else:
-            nxt_link = str(pgs[6].get_attribute('href'))
-            driver.get(nxt_link)
-            elems = scrape(driver)
-            clean_elems = clean(elems)
-            our_html_links.append(clean_elems)
-            for link in clean_elems:                                # loops through cleaned link list, writes to output file
-                if type(link) is str:
-                    output.write(link)
-                    output.write('\n')
-            pg_counter += 1
-            print('Size HTML list:  ',len(our_html_links))          # only do this if you want to keep track of the master list size
+            nxt_url = str(pgs[6].get_attribute('href'))
 
-    return our_html_links
+        driver.get(nxt_url)
+        elems = scrape(driver)
+        clean_elems = clean(elems)
+        urls.append(clean_elems)
+        # loops through cleaned url list, writes to output file
+        for url in clean_elems:
+            if type(url) is str:
+                output.write(url)
+                output.write('\n')
+        pg_counter += 1
+
+    return urls
+
 
 def scrape(driver):
     elems = driver.find_elements_by_xpath("//a[@href]")
     return elems
 
-####### Actual executed code is below ######
 
-file = open('html_list.txt','w')                                    # a file with that name must be already located in current dir (I think)
+@click.command()
+@click.option('-o', '--output_file', 'output_file', type=str, required=True,
+            help='name of the output file to contant the list of HTML urls')
+@click.option('-f', '--first_url', 'first_url', type=str, required=False,
+            default='https://scholar.google.com/scholar?start=0&q=flame+retardant&hl=en&as_sdt=0,48&as_ylo=2015',
+            show_default=True,
+            help='URL that the crawler should use as the search base')
+@click.option('-w', '--wait_time', 'wait_time', type=int, required=False,
+            default=6, show_default=True,
+            help='time in second between page fetches')
+@click.option('-m', '--max_iters', 'max_iters', type=int, required=False,
+            default=10, show_default=True,
+            help='maximum number of iterations (pages to fetch)')
+def selenium_extractor(output_file, first_url, wait_time, max_iters):
+    # NOTE, urls is constructed but nothing is done with it
+    with open(output_file, 'w') as f:
+        driver, urls = get_first_page(first_url, f)
+        # NOTE, subsequent calls to this, in a loop would increment ocunter and pg_start
+        urls.extend(
+                n_pages(driver, max_iters=10, counter=0, pg_start=1, 
+                        output=f, wait_time=wait_time)
+            )
 
-driver,our_html_links = first_page(first_link,file)
-our_html_links = n_pages(driver,10,0,1,file)                         # method takes (a) the driver (b) maximum pages to scrape (c) counter start, and (d) page start
+    print('collected %d total URLS' % (len(urls)))
+    print(urls)
 
-driver.quit()
+    driver.quit()
+
+
+if __name__ == '__main__':
+    selenium_extractor()
