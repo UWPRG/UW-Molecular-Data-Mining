016f69e58ef5058e08eed218451b46321b788454
Dave Beck <dacb@uw.edu>
2019-07-08 10:59:54 -0700

    converted docstrings from single quotes to triple doubles

--- scripts/scrape_sciencedirect_urls.py
+++ scripts/scrape_sciencedirect_urls.py
@@ -6,29 +6,29 @@ import bs4
 from bs4 import BeautifulSoup
 import time
 
-'''
+"""
 This code is used to scrape ScienceDirect of publication urls and write them to
 a text file in the current directory for later use.
 
 To use this code, go to ScienceDirect.com and search for the topic of interest.
 Then, copy the URL and paste it into terminal when prompted for user input.
 
-'''
+"""
 
 def scrape_page(driver):
-    '''
+    """
     This method finds all hrefs on webpage
-    '''
+    """
 
     elems = driver.find_elements_by_xpath("//a[@href]")
     return elems
 
 
 def clean(elems):
-    '''
+    """
     This method takes a list of scraped selenium web elements
     and filters/ returns only the hrefs leading to publications.
-    '''
+    """
 
     urls = []
     for elem in elems:
@@ -40,11 +40,11 @@ def clean(elems):
     return urls
 
 def build_annual_urls(first_url,year):
-    '''
+    """
     This method takes the first SD url and creates a list of
     urls which lead to the following pages on SD which will be
     scraped. The page list is for a given year.
-    '''
+    """
 
     page_urls = []
     for i in range(20):
@@ -55,11 +55,11 @@ def build_annual_urls(first_url,year):
     return page_urls
 
 def scrape_all(first_url,driver,year):
-    '''
+    """
     This method takes the first ScienceDirect url and navigates
     through all 60 pages of listed publications, scraping each url
     on each page. Returns a list of the urls. Scrapes all urls for a given year.
-    '''
+    """
     page_list = build_annual_urls(first_url,year)
     urls = []
     for page in page_list:
@@ -77,10 +77,10 @@ def scrape_all(first_url,driver,year):
 
 
 def proxify(scraped_urls,prefix):
-    '''
+    """
     This method takes a list of scraped urls and turns them into urls that
     go through the UW Library proxy so that all of them are full access.
-    '''
+    """
 
     proxy_urls = []
     for url in scraped_urls:
@@ -91,9 +91,9 @@ def proxify(scraped_urls,prefix):
     return proxy_urls
 
 def write_urls(urls,filename):
-    '''
+    """
     This method takes a list of urls and writes them to a desired text file.
-    '''
+    """
     file = open(filename,'w')
     for link in urls:
         file.write(link)
