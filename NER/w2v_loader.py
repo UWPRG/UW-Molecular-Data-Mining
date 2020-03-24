fd89617a71b7931f65939a62e2d0267cc7e0178c
Dave Juergens <davidcj@uw.edu>
2020-03-24 12:34:44 -0700

    Added function to append dfs without clobbering excel

--- NER/w2v_loader.py
+++ NER/w2v_loader.py
@@ -255,7 +255,82 @@ def make_ner_sheet(journal_directory, retrieval_type='description', years='all',
         sheet_number += 1
 
 
+def append_df_to_excel(filename, df, sheet_name='Sheet1', startrow=0, startcol=None,
+                       truncate_sheet=False,
+                       **to_excel_kwargs):
+    """
+    Append a DataFrame [df] to existing Excel file [filename]
+    into [sheet_name] Sheet.
+    If [filename] doesn't exist, then this function will create it.
 
+    Parameters:
+      filename : File path or existing ExcelWriter
+                 (Example: '/path/to/file.xlsx')
+      df : dataframe to save to workbook
+      sheet_name : Name of sheet which will contain DataFrame.
+                   (default: 'Sheet1')
+      startrow : upper left cell row to dump data frame.
+                 Per default (startrow=None) calculate the last row
+                 in the existing DF and write to the next row...
+
+      startcol : upper left cell column to dump data frame.
+
+
+      truncate_sheet : truncate (remove and recreate) [sheet_name]
+                       before writing DataFrame to Excel file
+      to_excel_kwargs : arguments which will be passed to `DataFrame.to_excel()`
+                        [can be dictionary]
+
+    Returns: None
+    """
+    from openpyxl import load_workbook
+
+    import pandas as pd
+
+    # ignore [engine] parameter if it was passed
+    if 'engine' in to_excel_kwargs:
+        to_excel_kwargs.pop('engine')
+
+    writer = pd.ExcelWriter(filename, engine='openpyxl')
+
+    # Python 2.x: define [FileNotFoundError] exception if it doesn't exist
+    try:
+        FileNotFoundError
+    except NameError:
+        FileNotFoundError = IOError
+
+
+    try:
+        # try to open an existing workbook
+        writer.book = load_workbook(filename)
+
+        if startcol is None and sheet_name in writer.book.sheetnames:
+            startcol = writer.book[sheet_name].max_col
+
+        # truncate sheet
+        if truncate_sheet and sheet_name in writer.book.sheetnames:
+            # index of [sheet_name] sheet
+            idx = writer.book.sheetnames.index(sheet_name)
+            # remove [sheet_name]
+            writer.book.remove(writer.book.worksheets[idx])
+            # create an empty sheet [sheet_name] using old index
+            writer.book.create_sheet(sheet_name, idx)
+
+        # copy existing sheets
+        writer.sheets = {ws.title:ws for ws in writer.book.worksheets}
+    except FileNotFoundError:
+        # file does not exist yet, we will create it
+        pass
+
+
+    if startcol is None:
+        startcol = 0
+
+    # write out the new sheet
+    df.to_excel(writer, sheet_name, startrow=startrow, startcol=startcol, **to_excel_kwargs)
+
+    # save the workbook
+    writer.save()
 
 
 
