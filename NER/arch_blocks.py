62e8af3609d6d962f443f924371bf6482f17de25
Dave Juergens <davidcj@uw.edu>
2020-03-30 13:51:13 -0700

    Committing arch_blocks.py before adding w2v to it
    At this point, the CharLSTM in arch_blocks.py works for training.
    There is no train/val split yet, and there is no use of w2v embeddings.
    
    Moving forward from this point, going to try and use w2v_embeddings to train
    and test, rather than pytorchs random embeddings.

--- NER/arch_blocks.py
+++ NER/arch_blocks.py
@@ -24,13 +24,13 @@ def prepare_sequence(sequence, to_idx, level):
         torch.tensor : A tensor containing the feature mappings for every entry in seq
     """
     if level == 'word':
-        idxs = [to_idx[word] for word in sequence] # a list of each word being mapped to it's representation
+        idxs = [to_idx[str(word)] for word in sequence] # a list of each word being mapped to it's representation
         return torch.tensor(idxs, dtype=torch.long)
 
     elif level == 'char':
         char_idxs_for_all_words = []
         for word in sequence:
-            idxs = [to_idx[char] for char in word]
+            idxs = [to_idx[char] for char in str(word)]
             char_idxs_for_all_words.append(idxs)
         return char_idxs_for_all_words
 
@@ -49,7 +49,7 @@ class LinearModel(nn.Module):
         output = self.linear(input)
         return output
 
-class CharLSTMTagger(nn.Module):
+class CharLSTM(nn.Module):
 
     def __init__(self,
                  word_embedding_dim, word_hidden_dim,
@@ -57,7 +57,7 @@ class CharLSTMTagger(nn.Module):
                  word_vocab_size, char_vocab_size,
                  tagset_size):
 
-        super(CharLSTMTagger, self).__init__()
+        super(CharLSTM, self).__init__()
         self.word_hidden_dim = word_hidden_dim
         self.char_hidden_dim = char_hidden_dim
 
@@ -103,15 +103,13 @@ class CharLSTMTagger(nn.Module):
         return tag_scores
 
 
-class WordLSTMTagger(nn.Module):
+class WordLSTM(nn.Module):
 
     def __init__(self,
                  word_embedding_dim, word_hidden_dim,
-                 char_embedding_dim, char_hidden_dim,
-                 word_vocab_size, char_vocab_size,
-                 tagset_size):
+                 word_vocab_size, tagset_size):
 
-        super(WordLSTMTagger, self).__init__()
+        super(WordLSTM, self).__init__()
         self.word_hidden_dim = word_hidden_dim
 
         self.word_embeddings = nn.Embedding(word_vocab_size, word_embedding_dim)
@@ -125,23 +123,7 @@ class WordLSTMTagger(nn.Module):
 
     def forward(self, sentence_words, sentence_characters):
         # get the word embeddings for each word in sentence
-        # print('This is sentence used for word_embeddings(sentence_words)')
-        # print(sentence_words)
         word_embeds = self.word_embeddings(sentence_words)
-        # # print('This is word_embeds ', word_embeds)
-        # char_embeds = [self.char_embeddings(torch.tensor(word, dtype=torch.long)) for word in sentence_characters]
-
-
-        # # send all of the characters through the char_lstm, get out representation
-        # char_level_reprs_fwd = torch.zeros(len(sentence_words), 6)
-        # for i, embed_group in enumerate(char_embeds): # remember this is a list of lists
-        #     char_lstm_out, _ = self.char_lstm(embed_group.view(len(embed_group),1, -1))
-        #     last_rep = char_lstm_out[-1][-1]
-        #     char_level_reprs_fwd[i] = last_rep
-        #
-        # # concat the word level and character level representations
-        # word_embeds = torch.cat((word_embeds, char_level_reprs_fwd), 1)
-
 
         word_lstm_out, _ = self.word_lstm(word_embeds.view(len(sentence_words), 1, -1))
 
